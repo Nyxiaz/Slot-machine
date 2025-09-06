260f3c9252996c3dc9bb0791e898c99595bbbfd2
Nyxiaz <dishasingh3108@gmail.com>
2025-09-06 19:24:48 +0530

    added spinning of reels

--- project.js
+++ project.js
@@ -1,5 +1,23 @@
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+
+const SYMBOLS_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2 
+}
+
+
 
 // Creating a function to get deposit amount from the user 
 const deposit = () =>{
@@ -30,5 +48,76 @@ const getLines = () =>{
     }
 };
 
-const depositAmount = deposit();
+
+//Creating a function to get the bet amount from the user
+const getBet = (Balance, lines) =>{
+    while (true){
+        const bet = prompt("Enter the amount you want to bet on: ");
+        const numberBet = parseFloat(bet);
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > Balance/lines){
+            console.log("Invalid Bet Amount! Please try again.");
+        }
+        else{
+            return numberBet;
+        }
+    }
+};
+
+
+//Creating a function to spin the slot machine
+const spin = () =>{
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++){
+            symbols.push(symbol)
+        }
+    }
+
+    const reels = [];
+    for(let i = 0; i < COLS; i++){
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random()* reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex]
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        } 
+    }
+    return reels;
+};
+
+
+//creating a function to transpose the reels into rows
+const transpose = (reels) =>{
+    const rows = [];
+    for (let i = 0; i < ROWS; i ++){
+        rows.push([]);
+        for (let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
+
+//printing the rows
+const printRows = (rows) =>{
+    for (const row of rows){
+        let rowString= "";
+        for (const [i, symbol] of row.entries()){
+            rowString += symbol
+            if (i != row.length -1){
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+};
+
+let Balance = deposit();
 const numberLines = getLines();
+const bet = getBet(Balance, numberLines);
+const reels =spin();
+const rows = transpose(reels);
+printRows(rows);

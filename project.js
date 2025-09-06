24bea88363320260686eb84862a6a0bb092ccb63
Nyxiaz <dishasingh3108@gmail.com>
2025-09-06 15:33:48 +0530

    adding function in project.js to set the no. of lines

--- project.js
+++ project.js
@@ -1,12 +1,13 @@
-// Creating a function to get deposit amount from the user 
 const prompt = require("prompt-sync")();
 
+
+// Creating a function to get deposit amount from the user 
 const deposit = () =>{
     while (true){
         const depositAmount = prompt("Enter the amount you want to deposit: ");
         const numberDeposit = parseFloat(depositAmount);
         if (isNaN(numberDeposit) || numberDeposit <= 0){
-            console.log("Invalid input. Please try again.");
+            console.log("Invalid Amount! Please try again.");
         }
         else{
             return numberDeposit;
@@ -14,5 +15,20 @@ const deposit = () =>{
     }
 };
 
+
+//Creating a function to get the number of lines to bet on
+const getLines = () =>{
+    while (true){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberLines = parseFloat(lines);
+        if (isNaN(numberLines) || numberLines <= 0 || numberLines > 3){
+            console.log("Invalid number of Lines! Please try again.");
+        }
+        else{
+            return numberLines;
+        }
+    }
+};
+
 const depositAmount = deposit();
-console.log(depositAmount);
+const numberLines = getLines();
